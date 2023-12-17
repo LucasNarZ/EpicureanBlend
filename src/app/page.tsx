f45fd3fb0e0fe54bdb57497f73bf68f05bfb40d0
LucasNarZ <lucasnarlochz@gmail.com>
2023-12-17 18:11:29 -0300

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,24 @@
-"use client";
 
 import React from 'react';
-import { useState } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
-import { IngredientComponent } from './components/ingredient';
+
+import { Search_Ingredients } from './components/search_ingredients';
+import { RecipesList } from './components/recipesList';
+import { getRecipes } from './functions/getRecipes';
 
 
 
-export default function Home() {
-    const [ingredient, setIngredient] = useState<string>("");
-    const [ingredients, setIngredients] = useState<Array<string>>([]);
-
-    const deleteIngredient = (delIngredient:string) => {
-        const newIngredients = ingredients.filter((ingredient) => ingredient != delIngredient);
-        setIngredients(newIngredients);
-    }
-
-    const handleEnterKeyPress = (e:any) => {
-        if (e.key === 'Enter') {
-            setIngredients([...ingredients, ingredient]);
-            setIngredient("");
-        }
-    };
-
-
+export default async function Home(
+    {searchParams}: 
+    {searchParams:{ingredients:string}}) {
+    let recipes:any[] = await getRecipes(searchParams.ingredients);
     return (
-        <div className='w-[100vw] h-[100vh] bg-white'>
+        <div className='w-[100vw] h-auto min-h-[100vh] bg-white'>
             <header className='flex h-30 items-center'>
                 <img src="logo_size.jpg" alt="logo" className='w-30 justify-self-start ml-5'/>
-                <div className='flex w-full h-full flex-col'>
-                    <div className='h-10 max-w-[1000px] w-[80%] relative'>
-                        <input type="text" className='focus:border-blue-100 focus:outline-none bg-gray-50 w-full h-full justify-self-center border-[1px] border-blue-600  rounded text-black pl-5' onChange={(e) => setIngredient(e.target.value)} value={ingredient} onKeyDown={handleEnterKeyPress} />
-                        <SearchIcon className='absolute top-[50%] translate-y-[-50%] right-10 text-black cursor-pointer' onClick={() => {setIngredients([...ingredients, ingredient]);setIngredient("")}}/>
-                    </div>
-                    <div className='flex mt-3'>
-                        {ingredients.map(ingredient => {
-                            return(
-                                <IngredientComponent ingredient={ingredient} onClose={() => deleteIngredient(ingredient)}/>
-                            )
-                        })}
-                    </div>
-                </div>
-                
+                <Search_Ingredients />
+
             </header>
-            
+            <RecipesList recipes={recipes}/>
         </div>
     )
 }
